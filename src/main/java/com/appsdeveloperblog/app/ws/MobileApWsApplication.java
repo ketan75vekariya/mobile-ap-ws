f89b3ed945e0eee2302491a986152d4f917a47d5
ketvek947 <ketvek947@gmail.com>
2025-12-03 10:53:09 +0000

    Frist commit and created user controller

---
+++ src/main/java/com/appsdeveloperblog/app/ws/MobileApWsApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileApWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileApWsApplication.class, args);
+	}
+
+}
