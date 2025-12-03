47f788f0c6d452f5532e8522f536d7d3d31a39d9
ketvek947 <ketvek947@gmail.com>
2025-12-03 13:23:44 +0000

    implemented user signup

---
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.appsdeveloperblog.app.ws.service.impl;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+import com.appsdeveloperblog.app.ws.repository.UserRepository;
+import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.Utils;
+import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	Utils utils;
+	
+	@Override
+	public UserDto createUser(UserDto user) {
+		if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Recored already exists");
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(user, userEntity);
+		userEntity.setEncryptedPassword("test");
+		String publicUserId = utils.generateUserId(30);
+		userEntity.setUserId(publicUserId);
+		UserEntity storedUserDetails = userRepository.save(userEntity);
+		UserDto returnValue = new UserDto();
+		BeanUtils.copyProperties(storedUserDetails, returnValue);
+		return returnValue;
+	}
+
+}
