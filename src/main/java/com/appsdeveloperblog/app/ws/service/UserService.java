81171764dcd28f4dfe624193b2c19682acc34f5d
ketvek947 <ketvek947@gmail.com>
2025-12-06 13:26:52 +0000

    Implemented Pagination and Get Users API Call

--- src/main/java/com/appsdeveloperblog/app/ws/service/UserService.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/UserService.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.app.ws.service;
 
+import java.util.List;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
@@ -11,5 +13,6 @@ public interface UserService extends UserDetailsService{
 	UserDto getUserByUserId(String userId);
 	UserDto updateUser(String userId, UserDto user);
 	void deleteUser(String userId);
+	List<UserDto> getUser(int page, int limit);
 
 }
--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.appsdeveloperblog.app.ws.service.impl;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -90,6 +93,22 @@ public class UserServiceImpl implements UserService {
 			throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
 		userRepository.delete(userEntity);
 		
+	}
+
+	@Override
+	public List<UserDto> getUser(int page, int limit) {
+		List<UserDto> returnValue = new ArrayList<>();
+		if(page>0) page = page-1;
+		PageRequest pageableRequest = PageRequest.of(page, limit);
+		Page<UserEntity> usersPage = userRepository.findAll(pageableRequest);
+		List<UserEntity> users = usersPage.getContent();
+		
+		for (UserEntity userEntity : users) {
+			UserDto userDto = new UserDto();
+			BeanUtils.copyProperties(userEntity, userDto);
+			returnValue.add(userDto);
+		}
+		return returnValue;
 	}
 
 }
