87f801403bcde86459ce7fbaa48b13965f6b2016
ketvek947 <ketvek947@gmail.com>
2025-12-06 12:07:59 +0000

    Implemented Update User Details API Call

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -48,9 +48,18 @@ public class UserController {
 		return returnValue;
 	}
 	
-	@PutMapping
-	public String updateUser() {
-		return "Update Users was called";
+	@PutMapping(path="/{id}")
+	public UserRest updateUser(@PathVariable String id, @RequestBody UserDetailsRequestModel userDetails) {
+		UserRest returnValue = new UserRest();
+		if(userDetails.getFirstName().isEmpty()
+				|| userDetails.getLastName().isEmpty()
+				|| userDetails.getEmail().isEmpty())
+			throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
+		UserDto userDto = new UserDto();
+		BeanUtils.copyProperties(userDetails, userDto);
+		UserDto updatedUser = userService.updateUser(id,userDto);
+		BeanUtils.copyProperties(updatedUser, returnValue);
+		return returnValue;
 	}
 	
 	@DeleteMapping
